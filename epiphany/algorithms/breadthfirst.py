c8895968877249c00dffb9a580052debac53d3e6
Lotte Notenboom <lottenotenboom@MacBook-Pro-van-Lotte.local>
2022-06-19 20:13:52 +0200

    added comments to breadthfirst.py

--- epiphany/algorithms/breadthfirst.py
+++ epiphany/algorithms/breadthfirst.py
@@ -1,6 +1,6 @@
-# state saving
-# pickle.dump(object, file_path)
-# variabele = pickle,load(file_path)
+'''
+Breadth First Algorithm for the Protein Folding Problem in the HP Lattice Model
+'''
 
 import random
 import queue
@@ -8,6 +8,7 @@ import copy
 
 from classes.protein import Protein
 from classes.amino import Amino
+
 
 class BreadthFirst():
 
@@ -20,16 +21,16 @@ class BreadthFirst():
 
     def run(self):
 
-        # Difference between 2D and 3D
+        # Different direction options for 2D and 3D solution
         if self.dimension == 2:
             directions = [1, -1, 2, -2]
         elif self.dimension == 3:
             directions = [1, -1, 2, -2 ,3, -3]
 
-        # Copy protein
+        # Create protein to build new states with
         first_protein = copy.deepcopy(self.protein_obj)
 
-        # Place first and second amino on grid
+        # Place first and second amino of protein on grid, to eliminate protein rotation
         first_protein.assign_coordinates([[0, 0, 0, 0]])
         first_protein.assign_coordinates([[1, 1, 0, 0]])
 
@@ -37,38 +38,45 @@ class BreadthFirst():
         main_queue = queue.Queue()
         main_queue.put(first_protein)
 
-        # Continue while there are still branches
+        # Keep excecuting algorithm until all possible solutions have been generated
         while not main_queue.empty():
 
-            # Get conformation that's next in line from queue, partial_conformation needs to change format
+            # Get next state
             protein = main_queue.get()
 
-            # Update id of amino in chain
+            # Get the id of current amino node
             index = self.get_index(protein)
 
+            # If the last amino in protein is reached, update scores and quit
             if index == "last_amino_acid":
                 break
                 print(index, "index")
 
-            # Check every possible move direction
+            # Create a new state for each possible move for next amino
             for direction in directions:
 
-                # Create new protein object
+                # Create new protein object for direction
                 partial_protein = copy.deepcopy(protein)
 
+                # Get details about current amino node
                 amino_node = partial_protein.view_protein()[index]
 
-                # Get coordinates of amino acid
+                # Assign coordinates of amino node
                 x = amino_node[1]
                 y = amino_node[2]
                 z = amino_node[3]
 
+                # Place next amino node on grid if possible, based on direction
                 if direction == -1 and (x - 1, y, z) not in partial_protein.filled_coordinates():
+
+                    # Update coordinates of next amino node
                     x = x - 1
                     partial_protein.assign_coordinates([[index + 1, x, y, z]])
 
+                    # Get the current score of partial conformation
                     score = self.get_score(partial_protein)
 
+                    # Add partial conformation to queue to build further upon
                     child = copy.deepcopy(partial_protein)
                     main_queue.put(child)
 
@@ -129,10 +137,15 @@ class BreadthFirst():
 
                     continue
 
+        # If all amino nodes have been placed, return the placement with the best score
+        print(self.best_placement.view_protein())
         return self.best_placement
 
     def get_index(self, protein_obj):
-
+        '''
+        Returns the index or id of the last placed amino node
+        If the current node is last node of protein, return last node message
+        '''
         last_amino = False
         amino_index = 0
 
@@ -149,7 +162,9 @@ class BreadthFirst():
 
 
     def get_score(self, protein_obj):
-
+        '''
+        Returns score of partial conformation
+        '''
         score = protein_obj.reward()
 
         if score <= self.best_score:
@@ -157,4 +172,7 @@ class BreadthFirst():
             self.best_placement = protein_obj
 
     def score(self):
+        '''
+        Returns the best score of partial conformations overall
+        '''
         return self.best_score
