796d251cec276c671f7f61caf9b91f591d6569b9
lotte128 <59421379+lotte128@users.noreply.github.com>
2022-07-02 21:33:36 +0200

    Update main.py

--- final_product/main.py
+++ final_product/main.py
@@ -1,7 +1,5 @@
 """
-This programm allows you to choose 1 out of 9 proteïns that you can fold.
-Than it allows you to choose the algorithm that you want to use and the amount of iterations.
-To choose an option, just enter the right integer.
+Choose protein string to fold and algorithm to use to fold it
 """
 
 import csv
@@ -21,10 +19,11 @@ from code.tools.plotters import plot_3d
 from code.classes.protein_model import Model
 
 if __name__ == "__main__":
-    print("Hello and welcome to our program!")
+    print("Hello and welcome to our protein folding program!\n")
 
+    # Protein string selection
     while True:
-        string_number = input("please, enter a protein string number (int: 1-9): \n")
+        string_number = input("1 HHPHHHPH\n2 HHPHHHPHPHHHPH\n3 HPHPPHHPHPPHPHHPPHPH\n4 PPPHHPPHHPPPPPHHHHHHHPPHHPPPPHHPPHPP\n5 HHPHPHPHPHHHHPHPPPHPPPHPPPPHPPPHPPPHPHHHHPHPHPHPHH\n6 PPCHHPPCHPPPPCHHHHCHHPPHHPPPPHHPPHPP\n7 CPPCHPPCHPPCPPHHHHHHCCPCHPPCPCHPPHPC\n8 HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH\n9 HCPHPHPHCHHHHPCCPPHPPPHPPPPCPPPHPPPHPHHHHCHPHPHPHH\nEnter a protein string number (int: 1-9): \n")
 
         if string_number.isnumeric():
             break
@@ -32,29 +31,28 @@ if __name__ == "__main__":
     str(string_number)
     string = load_protein("data/proteins.txt", string_number)
 
-    # Create Model
+    # Create protein model
     protein_model = Model(string)
 
-
-    # Choose your kind of algorithm.
+    # Choose algorithm
     while True:
-        print("Do you want to use a constructive or a iterative algorithm? \n",
-                "1. constructive \n",
-                "2. iterative \n")
+        print("Do you want to use a constructive or an iterative algorithm?\n",
+                "1. Constructive \n",
+                "2. Iterative \n")
         con_or_it = input("> ")
-        con_or_it =  int(con_or_it)
+        con_or_it = int(con_or_it)
 
         if con_or_it >= 1 and con_or_it <= 2:
             break
 
-    # choose your constructive algorithm.
+    # Constructive
     if con_or_it == 1:
         while True:
-            print("Now please, enter an algorithm you want to use. The options are: \n",
-                "1. Depth first \n",
-                "2. Breadth first \n",
-                "3. Branch and bound \n",
-                "4. Beam breadth \n",
+            print("Select an algorithm you would like to use. The options are: \n",
+                "1. Depth First \n",
+                "2. Breadth First \n",
+                "3. Branch And Bound \n",
+                "4. Beam Breadth Search \n",
                 "5. Randomize \n",
                 "6. Greedy \n")
 
@@ -63,37 +61,38 @@ if __name__ == "__main__":
             if alg >= 1 and alg <= 6:
                 break
 
-        # choose if you want to fold in 2D or 3D.
+        # Use 2D grid or 3D grid
         while True:
-            print("Now please, if you want to fold the protein in 2d or 3d. \n",
-                "2. 2d \n",
-                "3. 3d \n")
+            print("Do you want to fold the protein in 2D or in 3D?. \n",
+                "2. 2D \n",
+                "3. 3D \n")
 
             dimension = input("> ")
             dimension = int(dimension)
             if dimension == 2 or dimension == 3:
                 break
 
+    # Iterative
     elif con_or_it == 2:
         while True:
-            print("Now please, enter an algorithm you want to use. The options are: \n",
-                "7. Hill climber \n",
-                "8. Simulated annealing \n")
+            print("Select an algorithm you would like to use. The options are: \n",
+                "7. Hill Climber \n",
+                "8. Simulated Annealing \n")
             alg = input("> ")
             alg = int(alg)
             if alg == 7 or alg == 8:
                 break
 
-        # choose the amount of iterations
+        # Amount of iterations
         while True:
-            iterations = input("please, enter the amount of iterations you want: \n")
+            iterations = input("Enter the amount of iterations you would like to use: \n")
             iterations = int(iterations)
             if iterations > 0:
                 break
 
         randomize(protein_model)
 
-    # assign chosen algorithm
+    # Assign chosen algorithm
     if alg == 1:
         algorithm = DepthFirst(protein_model, dimension)
 
@@ -113,12 +112,12 @@ if __name__ == "__main__":
         algorithm = Greedy(protein_model, dimension)
 
     elif alg == 7:
-            algorithm = RandomClimber(protein_model)
+        algorithm = RandomClimber(protein_model)
 
     elif alg == 8:
         algorithm = SimulatedAnnealing(protein_model)
 
-    # Run algorithm
+    # Run the algorithm
     if con_or_it == 1:
         if alg != 5:
             solution = algorithm.run()
@@ -127,8 +126,8 @@ if __name__ == "__main__":
         for i in range(1):
             solution = algorithm.run(iterations)
 
-
+    # Print score to terminal
     print("Score:", solution.current_score())
 
-    # Plot solution
-    plot_3d(solution, "mand")
+    # Plot a visualization of the solution
+    plot_3d(solution, "plot_visualization")
