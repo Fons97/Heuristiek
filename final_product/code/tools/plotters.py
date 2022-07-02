cd6a8d8c4537de5ea41a8ae764955550b664cdfc
lotte128 <59421379+lotte128@users.noreply.github.com>
2022-07-02 22:49:26 +0200

    Update plotters.py

--- final_product/code/tools/plotters.py
+++ final_product/code/tools/plotters.py
@@ -6,33 +6,22 @@ from code.classes.protein_model import Model
 from code.algorithms.climber import RandomClimber
 
 
-def make_histogram(model: Model, scoring: list[int, ...], filename: str) -> None:
-
-    scores = scoring
-
-    plt.hist(scores, bins=10)
-    plt.show()
-
 def plot_3d(model: Model, filename: str) -> None:
 
     # Matplotlib
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
 
-    # amino_list = model.protein.values()
-    # print(amino_list)
-    # create list to be filled with coordinates of special_connections
+    # Create list to be filled with coordinates of special_connections
     special_list = []
 
     x_values = []
     y_values = []
     z_values = []
 
-    # hlabel = ['H', model.protein]
-
     for key, amino in model.protein.items():
 
-        # Plots amino nodes on 3D graph
+        # Plots amino nodes on 3D grid
         if amino[0] == 'H':
             special_list.append([key, amino[1], amino[2], amino[3]])
             ax.scatter(amino[1], amino[2], amino[3], s=50, c='red', zorder=2, label=[key, 'H'])
@@ -49,7 +38,7 @@ def plot_3d(model: Model, filename: str) -> None:
         y_values.append(amino[2])
         z_values.append(amino[3])
 
-
+    # Set values for axis, labels, title, and legend
     ax.set_xticks(np.arange(min(x_values)-1, max(x_values)+1, 1.0))
     ax.set_yticks(np.arange(min(y_values)-1, max(y_values)+1, 1.0))
     ax.set_zticks(np.arange(min(z_values)-1, max(z_values)+1, 1.0))
@@ -63,19 +52,20 @@ def plot_3d(model: Model, filename: str) -> None:
     ax.legend(handles=[mpatches.Patch(color='red', label='H'),
                        mpatches.Patch(color='blue', label='P'),
                        mpatches.Patch(color='green', label='C')])
-
+    
+    # Line between amino acids 
     ax.plot(x_values, y_values, z_values, linestyle="solid",
     linewidth=1.2, c="grey", zorder=1)
 
     # Needed to prevent a dotted line beeing drawn twice
-    counting_variable = 0
+    counter = 0
 
-    # This for loop draws the dotted lines
+    # Draws dotted lines between amino acids that generate points
     for amino in special_list:
 
-        counting_variable += 1
+        counter += 1
 
-        for compare_amino in special_list[counting_variable:]:
+        for compare_amino in special_list[counter:]:
 
             if amino[0] - compare_amino[0] <= 1 and amino[0] - compare_amino[0] >= -1:
                 continue
@@ -97,5 +87,5 @@ def plot_3d(model: Model, filename: str) -> None:
                 z_axis_interactions, linestyle="dotted",
                 linewidth=3, c="green", zorder=1)
 
-
+    # Output plot to image 
     plt.show()
